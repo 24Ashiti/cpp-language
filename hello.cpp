5b76f43f659bfb2fbceff169f45fa75729cf32fb
Ashiti <ashitidhanani@icloud.com>
2023-07-12 22:46:01 -0700

    cpp language

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"hello world";
+    cout<<"\n enter a:";
+    cin>>a;
+    cout<<"enter b:";
+    cin>>b;
+    cout<<"product is:"<<a*b;
+}
